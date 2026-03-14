3cbf0cca1ffd4e5c453b1aba3f9f228203d4c0d7
Victornck <193505785+Victornck@users.noreply.github.com>
2026-03-14 09:50:39 -0300

    fix: Change base URL

--- admindesk-front/src/services/api.js
+++ admindesk-front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://admindesk-8oea.onrender.com'
+  baseURL: 'https://admindesk-8oea.onrender.com'
 });
 
 api.interceptors.request.use(config => {
